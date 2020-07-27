8c88657e0434c36e0b9dca94fdadd53b58ebd02b
SaamiSulaiman <saamisulaiman@gmail.com>
2020-07-27 08:27:24 +0700

    git commit

---
+++ android/app/src/main/kotlin/com/example/flutterkartunama/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterkartunama
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: false,
+          title: Text('Kartu Nama'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 80,
+              backgroundImage: NetworkImage(
+                  'https://cdn.iconscout.com/icon/free/png-512/account-profile-avatar-man-circle-round-user-30452.png'),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Saami Sulaiman',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    'saamisulaiman@gmail.com',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    '+6281298265060',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
